de4a16f2865ebfda7129d3e215e0c99f147fec0e
Mihaela <mihaela.staraj@gmail.com>
2022-12-14 23:29:11 +0100

    react state, events

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  // seperate states for each component instance
+  // on change only that specific instance will be re-evaluated
+  // const [title, setTitle] = useState(props.title); // props.title - default value
+  // const clickHandler = () => {
+  //   setTitle("Updated!");
+  // };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -11,6 +17,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      {/* <button onClick={clickHandler}>Change title</button>  */}
     </Card>
   );
 };
